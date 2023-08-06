f25f9c65400cfe2e4af89ced3e04fdf470cf0db4
Com1 Software, Inc <information@com1software.com>
2023-08-06 15:28:25 -0400

    Create main.go

---
+++ Server/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os/exec"
+	"runtime"
+	"time"
+)
+
+//-------------------------------------------------------------------------
+func main() {
+	agent := SSE()
+	fmt.Println("Test Server Sent Events ")
+	fmt.Printf("Operating System : %s\n", runtime.GOOS)
+
+	go func() {
+		for {
+			time.Sleep(time.Second * 2)
+			event := fmt.Sprintf(" %v\n", time.Now())
+			agent.Notifier <- []byte(event)
+		}
+	}()
+
+	http.ListenAndServe("localhost:8080", agent)
+
+}
+
+type Agent struct {
+	Notifier    chan []byte
+	newuser     chan chan []byte
+	closinguser chan chan []byte
+	user        map[chan []byte]bool
+}
+
+func SSE() (agent *Agent) {
+	agent = &Agent{
+		Notifier:    make(chan []byte, 1),
+		newuser:     make(chan chan []byte),
+		closinguser: make(chan chan []byte),
+		user:        make(map[chan []byte]bool),
+	}
+	go agent.listen()
+	return
+}
+
+func (agent *Agent) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	flusher, ok := rw.(http.Flusher)
+	if !ok {
+		http.Error(rw, "Error ", http.StatusInternalServerError)
+		return
+	}
+	rw.Header().Set("Content-Type", "text/event-stream")
+	rw.Header().Set("Cache-Control", "no-cache")
+	rw.Header().Set("Connection", "keep-alive")
+	rw.Header().Set("Access-Control-Allow-Origin", "*")
+	mChan := make(chan []byte)
+	agent.newuser <- mChan
+	defer func() {
+		agent.closinguser <- mChan
+	}()
+	notify := req.Context().Done()
+	go func() {
+		<-notify
+		agent.closinguser <- mChan
+	}()
+	for {
+		fmt.Fprintf(rw, "%s", <-mChan)
+		flusher.Flush()
+	}
+
+}
+
+func (agent *Agent) listen() {
+	for {
+		select {
+		case s := <-agent.newuser:
+			agent.user[s] = true
+		case s := <-agent.closinguser:
+			delete(agent.user, s)
+		case event := <-agent.Notifier:
+			for userMChan, _ := range agent.user {
+				userMChan <- event
+			}
+		}
+	}
+
+}
